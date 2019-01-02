42113a2bcb7ac797e5f06abdadedce572ae30a9e
mu <mayank@einsite.io>
2019-01-02 23:20:23 +0530

    introduce progress and options dialog

--- airdialog/src/main/java/mumayank/com/airdialog/AirDialog.kt
+++ airdialog/src/main/java/mumayank/com/airdialog/AirDialog.kt
@@ -1,8 +1,9 @@
 package mumayank.com.airdialog
 
 import android.app.Activity
+import android.app.Dialog
+import android.app.ProgressDialog
 import android.support.v7.app.AlertDialog
-import android.widget.Button
 import java.lang.ref.WeakReference
 
 class AirDialog(
@@ -13,10 +14,10 @@ class AirDialog(
 ) {
     class Button(val textOnButton: String, val onClick: () -> Unit)
 
-    private var alertDialog: AlertDialog? = null
+    private var dialog: Dialog? = null
 
     fun dismiss() {
-        alertDialog?.dismiss()
+        dialog?.dismiss()
     }
 
     fun show(
@@ -27,82 +28,163 @@ class AirDialog(
     ) {
         val activityWeakReference = WeakReference<Activity>(activity)
         val alertDialogBuilder = AlertDialog.Builder(activity)
-
         setTitle(alertDialogBuilder)
-        setMessage(alertDialogBuilder, message)
+        setMessage(alertDialogBuilder, message = message)
         setIcon(alertDialogBuilder)
         setCancelable(alertDialogBuilder)
-        setButton1(alertDialogBuilder, activityWeakReference, airButton1)
-        setButton2(alertDialogBuilder, activityWeakReference, airButton2)
-        setButton3(alertDialogBuilder, activityWeakReference, airButton3)
+        setButton1(alertDialogBuilder, activityWeakReference = activityWeakReference, airButton1 = airButton1)
+        setButton2(alertDialogBuilder, activityWeakReference = activityWeakReference, airButton2 = airButton2)
+        setButton3(alertDialogBuilder, activityWeakReference = activityWeakReference, airButton3 = airButton3)
         setAtLeastOneButtonIfNoButtonsSpecified(alertDialogBuilder, activityWeakReference, airButton1, airButton2, airButton3)
+        showDialog(alertDialogBuilder, activityWeakReference = activityWeakReference)
+    }
 
+    fun showNoButtons(
+        message: String = ""
+    ) {
+        val activityWeakReference = WeakReference<Activity>(activity)
+        val alertDialogBuilder = AlertDialog.Builder(activity)
+        setTitle(alertDialogBuilder)
+        setMessage(alertDialogBuilder, message = message)
+        setIcon(alertDialogBuilder)
+        setCancelable(alertDialogBuilder)
+        showDialog(alertDialogBuilder, activityWeakReference = activityWeakReference)
+    }
 
+    interface ShowOptionsCallback {
+        fun onOptionSelected(selectedOptionIndex: Int)
+        fun onCancelled()
+    }
 
+    fun showOptions(
+        charSequenceArray: Array<CharSequence>,
+        airButton1: Button? = null,
+        airButton2: Button? = null,
+        airButton3: Button? = null,
+        onOptionsCallback: ShowOptionsCallback,
+        defaultSelectedOptionIndex: Int = 0
+    ) {
+        val activityWeakReference = WeakReference<Activity>(activity)
+        val alertDialogBuilder = AlertDialog.Builder(activity)
+        setTitle(alertDialogBuilder)
+        alertDialogBuilder.setSingleChoiceItems(charSequenceArray, defaultSelectedOptionIndex) { d, selectedOptionIndex ->
+            onOptionsCallback.onOptionSelected(selectedOptionIndex)
+            d.dismiss()
+        }
+        alertDialogBuilder.setOnCancelListener {
+            onOptionsCallback.onCancelled()
+        }
+        setIcon(alertDialogBuilder)
+        setCancelable(alertDialogBuilder)
+        setButton1(alertDialogBuilder, activityWeakReference = activityWeakReference, airButton1 = airButton1)
+        setButton2(alertDialogBuilder, activityWeakReference = activityWeakReference, airButton2 = airButton2)
+        setButton3(alertDialogBuilder, activityWeakReference = activityWeakReference, airButton3 = airButton3)
+        showDialog(alertDialogBuilder, activityWeakReference = activityWeakReference)
+    }
 
+    fun showProgress(
+        message: String = "",
+        airButton1: Button? = null,
+        airButton2: Button? = null,
+        airButton3: Button? = null
+    ) {
+        val activityWeakReference = WeakReference<Activity>(activity)
+        val progressDialog = ProgressDialog(activity)
+        setTitle(progressDialog = progressDialog)
+        setMessage(progressDialog = progressDialog, message = message)
+        setIcon(progressDialog = progressDialog)
+        setCancelable(progressDialog = progressDialog)
+        setButton1(progressDialog = progressDialog, activityWeakReference = activityWeakReference, airButton1 = airButton1)
+        setButton2(progressDialog = progressDialog, activityWeakReference = activityWeakReference, airButton2 = airButton2)
+        setButton3(progressDialog = progressDialog, activityWeakReference = activityWeakReference, airButton3 = airButton3)
+        showDialog(progressDialog = progressDialog, activityWeakReference = activityWeakReference)
+    }
 
-
-
-
-
-
+    private fun showDialog(alertDialogBuilder: AlertDialog.Builder? = null, progressDialog: ProgressDialog? = null, activityWeakReference: WeakReference<Activity>) {
         if (activity.isFinishing == false) {
             if (activityWeakReference.get() != null) {
-                alertDialog = alertDialogBuilder.create()
-                alertDialog?.show()
+                if (alertDialogBuilder != null) {
+                    dialog = alertDialogBuilder.create()
+                    dialog?.show()
+                } else if (progressDialog != null) {
+                    dialog = progressDialog
+                }
+                dialog?.show()
             }
         }
     }
 
-    private fun setTitle(alertDialogBuilder: AlertDialog.Builder) {
+    private fun setTitle(alertDialogBuilder: AlertDialog.Builder? = null, progressDialog: ProgressDialog? = null) {
         if (title != "") {
-            alertDialogBuilder.setTitle(title)
+            alertDialogBuilder?.setTitle(title)
+            progressDialog?.setTitle(title)
         }
     }
 
-    private fun setMessage(alertDialogBuilder: AlertDialog.Builder, message: String) {
+    private fun setMessage(alertDialogBuilder: AlertDialog.Builder? = null, progressDialog: ProgressDialog? = null, message: String) {
         if (message != "") {
-            alertDialogBuilder.setMessage(message)
+            alertDialogBuilder?.setMessage(message)
+            progressDialog?.setMessage(message)
         }
     }
 
-    private fun setIcon(alertDialogBuilder: AlertDialog.Builder) {
+    private fun setIcon(alertDialogBuilder: AlertDialog.Builder? = null, progressDialog: ProgressDialog? = null) {
         if (iconDrawableId != null) {
-            alertDialogBuilder.setIcon(iconDrawableId)
+            alertDialogBuilder?.setIcon(iconDrawableId)
+            progressDialog?.setIcon(iconDrawableId)
         }
     }
 
-    private fun setCancelable(alertDialogBuilder: AlertDialog.Builder) {
-        alertDialogBuilder.setCancelable(isCancelable)
+    private fun setCancelable(alertDialogBuilder: AlertDialog.Builder? = null, progressDialog: ProgressDialog? = null) {
+        alertDialogBuilder?.setCancelable(isCancelable)
+        progressDialog?.setCancelable(isCancelable)
     }
 
-    private fun setButton1(alertDialogBuilder: AlertDialog.Builder, activityWeakReference: WeakReference<Activity>, airButton1: Button?) {
+    private fun setButton1(alertDialogBuilder: AlertDialog.Builder? = null, progressDialog: ProgressDialog? = null, activityWeakReference: WeakReference<Activity>, airButton1: Button?) {
         if (airButton1 != null) {
-            alertDialogBuilder.setPositiveButton(airButton1.textOnButton) { _, _ ->
+            alertDialogBuilder?.setPositiveButton(airButton1.textOnButton) { _, _ ->
                 if (activityWeakReference.get() != null) {
-                    alertDialog?.dismiss()
+                    dialog?.dismiss()
+                    airButton1.onClick.invoke()
+                }
+            }
+            progressDialog?.setButton(Dialog.BUTTON_POSITIVE, airButton1.textOnButton) { _, _ ->
+                if (activityWeakReference.get() != null) {
+                    dialog?.dismiss()
                     airButton1.onClick.invoke()
                 }
             }
         }
     }
 
-    private fun setButton2(alertDialogBuilder: AlertDialog.Builder, activityWeakReference: WeakReference<Activity>, airButton2: Button?) {
+    private fun setButton2(alertDialogBuilder: AlertDialog.Builder? = null, progressDialog: ProgressDialog? = null, activityWeakReference: WeakReference<Activity>, airButton2: Button?) {
         if (airButton2 != null) {
-            alertDialogBuilder.setNegativeButton(airButton2.textOnButton) { _, _ ->
+            alertDialogBuilder?.setNegativeButton(airButton2.textOnButton) { _, _ ->
                 if (activityWeakReference.get() != null) {
-                    alertDialog?.dismiss()
+                    dialog?.dismiss()
+                    airButton2.onClick.invoke()
+                }
+            }
+            progressDialog?.setButton(Dialog.BUTTON_NEGATIVE, airButton2.textOnButton) { _, _ ->
+                if (activityWeakReference.get() != null) {
+                    dialog?.dismiss()
                     airButton2.onClick.invoke()
                 }
             }
         }
     }
 
-    private fun setButton3(alertDialogBuilder: AlertDialog.Builder, activityWeakReference: WeakReference<Activity>, airButton3: Button?) {
+    private fun setButton3(alertDialogBuilder: AlertDialog.Builder? = null, progressDialog: ProgressDialog? = null, activityWeakReference: WeakReference<Activity>, airButton3: Button?) {
         if (airButton3 != null) {
-            alertDialogBuilder.setNeutralButton(airButton3.textOnButton) { _, _ ->
+            alertDialogBuilder?.setNeutralButton(airButton3.textOnButton) { _, _ ->
                 if (activityWeakReference.get() != null) {
-                    alertDialog?.dismiss()
+                    dialog?.dismiss()
+                    airButton3.onClick.invoke()
+                }
+            }
+            progressDialog?.setButton(Dialog.BUTTON_NEUTRAL, airButton3.textOnButton) { _, _ ->
+                if (activityWeakReference.get() != null) {
+                    dialog?.dismiss()
                     airButton3.onClick.invoke()
                 }
             }
@@ -114,7 +196,7 @@ class AirDialog(
             val tempAirButton = AirDialog.Button("OK") {}
             alertDialogBuilder.setPositiveButton(tempAirButton.textOnButton) { _, _ ->
                 if (activityWeakReference.get() != null) {
-                    alertDialog?.dismiss()
+                    dialog?.dismiss()
                     tempAirButton.onClick.invoke()
                 }
             }
--- app/src/main/java/mumayank/com/airdialoglibraryproject/MainActivity.kt
+++ app/src/main/java/mumayank/com/airdialoglibraryproject/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import mumayank.com.airdialog.AirDialog
 
 class MainActivity : AppCompatActivity() {
@@ -11,25 +12,44 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        showDialog()
-    }
 
-    private fun showDialog() {
-        val a = AirDialog(
-            this,
-            "T",
-            "M",
-            R.drawable.notification_icon_background,
-            false,
-            AirDialog.Button("OK") {
-                showDialog()
-            },
-            AirDialog.Button("Cancel") {
-                Toast.makeText(this, "This is a toast", Toast.LENGTH_SHORT).show()
-            },
-            AirDialog.Button("Maybe") {
-                Toast.makeText(this, "This is a maybe button", Toast.LENGTH_SHORT).show()
-            }
-        )
+        button1.setOnClickListener {
+            AirDialog(this, "Hello", false).show(
+                "wha",
+                AirDialog.Button("OK") {
+                    Toast.makeText(this, "OK pressed", Toast.LENGTH_SHORT).show()
+                },
+                AirDialog.Button("CANCEL") {
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show()
+                }
+            )
+        }
+
+        button2.setOnClickListener {
+            AirDialog(this, "Some").showNoButtons("Please wait")
+        }
+
+        button3.setOnClickListener {
+            val airDialog = AirDialog(this, isCancelable = false).showProgress("Some message", airButton3 = AirDialog.Button("Cancel") {
+
+            })
+        }
+
+        button4.setOnClickListener {
+            AirDialog(this, "Choose:", false, android.R.drawable.ic_notification_clear_all).showOptions(arrayOf("One", "Two", "Three"),
+                airButton3 = AirDialog.Button("Cancel") {
+
+                },
+                onOptionsCallback = object: AirDialog.ShowOptionsCallback {
+                override fun onOptionSelected(selectedOptionIndex: Int) {
+                    Toast.makeText(this@MainActivity, selectedOptionIndex.toString(), Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onCancelled() {
+                    Toast.makeText(this@MainActivity, "Cancelled", Toast.LENGTH_SHORT).show()
+                }
+
+            }, defaultSelectedOptionIndex = 1)
+        }
     }
 }
